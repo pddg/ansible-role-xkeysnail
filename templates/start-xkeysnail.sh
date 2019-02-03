6ba0a6cf6aa7e150164733c9905281c8d84e3463
Pudding <s.kokuryo@gmail.com>
2019-02-03 12:01:51 +0900

    Use xkeysnail in user scope systemd service.

--- templates/start-xkeysnail.sh
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/env bash
-if [ -x {{ xkeysnail_path }} ]; then
-    xhost +SI:localuser:xkeysnail
-    sudo -u xkeysnail DISPLAY=:1 {{ xkeysnail_path }} {{ xkeysnail_config_dir }}/config.py &
-fi
